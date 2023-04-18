74a64638b2e50661e556f7d252f7f181d24bfce2
manuelcasas <128051762+manuelcasas23@users.noreply.github.com>
2023-04-18 18:55:41 +0200

    Archivos iniciales para el proyecto MakeCode

---
+++ main.ts
@@ -0,0 +1,16 @@
+IR.IR_callbackUser(function (código) {
+    if (código == 17) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CW, 50)
+    } else if (código == 18) {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 50)
+    } else if (código == 19) {
+        maqueen.motorRun(maqueen.Motors.M1, maqueen.Dir.CW, 25)
+    } else if (código == 20) {
+        maqueen.motorRun(maqueen.Motors.M2, maqueen.Dir.CW, 25)
+    } else {
+        maqueen.motorRun(maqueen.Motors.All, maqueen.Dir.CCW, 0)
+    }
+})
+basic.forever(function () {
+    basic.showNumber(IR.IR_read())
+})
